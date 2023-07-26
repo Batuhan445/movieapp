f65ca31efc0db83ccca437d2aa5d261ee3b21564
Batuhan445 <neomatrix44@hotmail.com>
2023-07-26 12:40:11 +0300

    created html

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            {children}
+        </body>
+
+    </html>
+  )
+}
+
+export default layout
