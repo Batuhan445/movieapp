31d04a20af9b4a1dc2cca74dfca570c7df2627a3
Batuhan445 <neomatrix44@hotmail.com>
2023-07-26 15:29:46 +0300

    navbar ui edited

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BiSearch } from "react-icons/bi";
 
-const header = () => {
-  return (
-    <div>header</div>
-  )
-}
+const Header = () => {
+    
 
-export default header
+    return (
+        <div className="flex items-center gap-3 h-20 p-5">
+            <div className="bg-amber-600 p-3 text-2xl font-bold rounded-lg">
+                MovieApp
+            </div>
+            <div className="flex items-center gap-2 border p-2 flex-1 rounded-lg">
+                <input
+                    className="outline-none flex-1"
+                    placeholder="Arama Yapınız"
+                    type="text"
+                />
+                <BiSearch className="cursor-pointer" size={25} />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
