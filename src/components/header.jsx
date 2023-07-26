411396c3fce6bf4e80d09807595be3b32f2fee7d
Batuhan445 <neomatrix44@hotmail.com>
2023-07-26 15:36:29 +0300

    created menu items and ui

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import { BiSearch } from "react-icons/bi";
+import MenuItem from "./MenuItem";
 
 const Header = () => {
-    
+    const menu = [
+        {
+            name: "Sign In",
+            url: "/login"
+        },
+        {
+            name: "About",
+            url: "/about"
+        },
+    ];
 
     return (
+
         <div className="flex items-center gap-3 h-20 p-5">
+
             <div className="bg-amber-600 p-3 text-2xl font-bold rounded-lg">
                 MovieApp
             </div>
+
             <div className="flex items-center gap-2 border p-2 flex-1 rounded-lg">
+
                 <input
                     className="outline-none flex-1"
                     placeholder="Arama Yapınız"
                     type="text"
                 />
+
                 <BiSearch className="cursor-pointer" size={25} />
             </div>
+
+        {
+            menu.map((mn, i) => (
+                <MenuItem mn={mn} key={i}/>
+            ))
+        }
+
         </div>
     );
 };
