a1641d64779423dac8fbd880577bb9da7dd01cb6
Batuhan445 <neomatrix44@hotmail.com>
2023-07-26 14:07:28 +0300

    added header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./globals.css"
+import Header from "@/components/header"
 
 const layout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+            <Header/>
             {children}
         </body>
 
