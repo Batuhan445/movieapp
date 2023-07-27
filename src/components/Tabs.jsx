d181fe25b7fc7ad0a1e12ce6a47288858b1bee39
Batuhan445 <neomatrix44@hotmail.com>
2023-07-27 14:39:04 +0300

    edited

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -28,7 +28,9 @@ const Tabs = () => {
     return (
         <div className='p-5 my-3 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7'>
             {tabs.map((tab, i) => (
-                <Link className={`hover:opacity-75 transition-opacity ${tab.url === genre ? "underline underline-offset-8 text-amber-600" : ""}`} href={`/?genre=${tab.url}`}>{tab.name}</Link>
+                <Link className={`hover:opacity-75 transition-opacity ${tab.url === genre ? "underline underline-offset-8 text-amber-600" : ""}`} href={`/?genre=${tab.url}`}>
+                    {tab.name}
+                </Link>
             ))}
         </div>
     )
