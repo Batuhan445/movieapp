40642c2193089c52ad926383aef7e59b17bdd2f3
Batuhan445 <neomatrix44@hotmail.com>
2023-07-27 16:55:18 +0300

    added film info

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const Movies = ({ dt }) => {
-  console.log(dt, "dt");
+  
   
   return (
-    <div className='min-w-[400px] h-[300px]'>
+
+    <div className='w-[400px] h-[300px] m-auto relative'>
       <img src={`https://image.tmdb.org/t/p/original/${dt?.backdrop_path || dt?.poster_path}`} alt="Movie Poster"/>
+
+      <div className='absolute bottom-0 p-3 flex flex-col justify-end opacity-0 text-center w-full h-full hover:opacity-100'>
+        <div>{dt.title}</div>
+        <div>{dt.release_date}</div>
+      </div>
+
     </div>
+
   );
 }
 
 export default Movies;
+
