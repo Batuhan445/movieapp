d13fbd8f046daf894e47af9256c197426a45e979
Batuhan445 <neomatrix44@hotmail.com>
2023-07-27 16:55:03 +0300

    fixed api problem

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Movies from '@/components/Movies';
 import React from 'react';
 
 const page = async ({ searchParams }) => {
-  const genreQuery = searchParams.genre ? "movie/" + searchParams.genre : "trending/all/day";
+  const genreQuery = searchParams.genre ? "movie/" + searchParams.genre : "movie/upcoming/";
   const apiKey = "1d5dad3345b663e244653304e46b496f";
   const apiUrl = `https://api.themoviedb.org/3/${genreQuery}?api_key=${apiKey}&language=en-US&page=1`;
 
@@ -10,15 +10,16 @@ const page = async ({ searchParams }) => {
     const res = await fetch(apiUrl);
     const data = await res.json();
 
-    console.log(data, "data");
-
     return (
-      <div>
+
+      <div className='flex items-center flex-wrap gap-3'>
         {data?.results?.map((dt, i) => (
           <Movies key={i} dt={dt} />
         ))}
       </div>
+
     );
+
   } catch (error) {
     console.error("Error fetching data:", error);
     // You might want to handle the error here appropriately
@@ -27,3 +28,5 @@ const page = async ({ searchParams }) => {
 };
 
 export default page;
+
+
