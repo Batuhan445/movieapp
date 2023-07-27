64743e6b19bb7103cd5b179c29806aaf07bd2dc7
Batuhan445 <neomatrix44@hotmail.com>
2023-07-27 11:59:17 +0300

    added tabs navbar

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,8 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Tabs = () => {
+
+    const tabs = [
+        {
+            name: "En Popüler",
+            url: "popular"
+        },
+        {
+            name: "Yeni Çıkanlar",
+            url: "latest"
+        },
+        {
+            name: "Çok Yakında",
+            url: "upcoming"
+        }
+    ]
+
+
+
   return (
-    <div>Tabs</div>
+    <div className='p-5 my-3 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7'>
+        {tabs.map((tab, i) =>(
+            <Link href={`/?genre=${tab.url}`}>{tab.name}</Link>
+        ))}
+    </div>
   )
 }
 
