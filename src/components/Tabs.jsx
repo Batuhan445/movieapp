3346b29c07c261a53846b65c0bd5ec894313a03c
Batuhan445 <neomatrix44@hotmail.com>
2023-07-27 12:10:25 +0300

    tabs navbar ui edited and added link

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,12 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
+import { useSearchParams } from "next/navigation"
 
 const Tabs = () => {
+
+    const searchParams = useSearchParams()
+    const genre = searchParams.get("genre")
 
     const tabs = [
         {
@@ -20,13 +25,13 @@ const Tabs = () => {
 
 
 
-  return (
-    <div className='p-5 my-3 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7'>
-        {tabs.map((tab, i) =>(
-            <Link href={`/?genre=${tab.url}`}>{tab.name}</Link>
-        ))}
-    </div>
-  )
+    return (
+        <div className='p-5 my-3 bg-gray-100 dark:bg-gray-900 flex items-center justify-center gap-7'>
+            {tabs.map((tab, i) => (
+                <Link className={`hover:opacity-75 transition-opacity ${tab.url === genre ? "underline underline-offset-8 text-amber-600" : ""}`} href={`/?genre=${tab.url}`}>{tab.name}</Link>
+            ))}
+        </div>
+    )
 }
 
 export default Tabs
