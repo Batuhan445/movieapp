cf2191c88d8d38624ad97de22b923652bbda1191
Batuhan445 <neomatrix44@hotmail.com>
2023-07-27 11:19:38 +0300

    added tabs page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./globals.css"
 import Header from "@/components/header"
 import Provider from './provider'
+import Tabs from '@/components/Tabs'
 
 const layout = ({children}) => {
   return (
@@ -9,6 +10,7 @@ const layout = ({children}) => {
         <body>
             <Provider>
             <Header/>
+            <Tabs/>
             {children}
             </Provider>
         </body>
