39425544d6b220a59b17c148eadb9ba72aa4ee84
Batuhan445 <neomatrix44@hotmail.com>
2023-07-31 17:25:21 +0300

    added search function

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import MenuItem from "./MenuItem";
 import ThemeComp from "./ThemeComp";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+    const [keyword, setKeyword] = useState("")
+    const router = useRouter();
+    
+
     const menu = [
         {
             name: "Sign In",
@@ -14,6 +20,12 @@ const Header = () => {
             url: "/about"
         },
     ];
+
+    const searchFunc = (e) => {
+        if(e.key === "Enter" && keyword.length >= 3) {
+            router.push(`/search/${keyword}`)
+        }
+    }
 
     return (
 
@@ -26,12 +38,14 @@ const Header = () => {
             <div className="flex items-center gap-2 border p-2 flex-1 rounded-lg">
 
                 <input
+                    onKeyDown={searchFunc}
+                    onChange={e => setKeyword(e.target.value)}
                     className="outline-none flex-1 bg-transparent"
                     placeholder="Arama Yapınız"
                     type="text"
                 />
 
-                <BiSearch className="cursor-pointer" size={25} />
+                <BiSearch className="cursor-pointer" size={25} onClick={searchFunc}/>
             </div>
 
             <ThemeComp/>
@@ -47,3 +61,5 @@ const Header = () => {
 };
 
 export default Header;
+
+
