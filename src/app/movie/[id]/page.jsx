7191632e5252bdf9fc57673ec3e75a6ee10787e2
Batuhan445 <neomatrix44@hotmail.com>
2023-08-01 17:50:30 +0300

    film detail fixed

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,49 +1,51 @@
-import React from 'react'
-import Image from 'next/image';
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
+const getMovie = (id) => {
+  const apiKey = "1d5dad3345b663e244653304e46b496f";
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=tr`
+  ).then((res) => res.json());
+};
 
-const getMovie = async (id) => {
-    const apiKey = "1d5dad3345b663e244653304e46b496f";
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=tr`)
-    return await res.json()
-}
+const Page = ({ params }) => {
+  const { id } = params;
+  const [movieDetail, setMovieDetail] = useState(null);
 
-const page = async ({params}) => {
-    const id = params.id
-    const movieDetail = await getMovie(id)
+  useEffect(() => {
+    getMovie(id)
+      .then((movie) => setMovieDetail(movie))
+      .catch((error) => console.error("Error fetching movie:", error));
+  }, [id]);
 
+  if (!movieDetail) {
+    return <div>Yükleniyor...</div>;
+  }
 
   return (
-    <div className="p-7 flex flex-col items-center">
+    <div className="flex flex-col items-center">
+      <div className="w-full min-h-screen relative rounded-xl overflow-hidden">
+        <Image
+          layout="fill"
+          objectFit="cover"
+          style={{ opacity: "0.5" }}
+          src={`https://image.tmdb.org/t/p/original/${
+            movieDetail?.backdrop_path || movieDetail?.poster_path
+          }`}
+          alt="Movie Poster"
+        />
 
-      <div className="flex flex-col md:flex-row items-center md:items-start">
-
-        {/* Image on the left */}
-        <div className="mb-4 md:mr-8">
-          <Image
-          width={1280}
-          height={720}
-            style={{borderRadius:"0.75rem"}}
-            src={`https://image.tmdb.org/t/p/original/${movieDetail?.backdrop_path || movieDetail?.poster_path}`}
-            alt="Movie Poster"
-          />
-        </div>
-
-        {/* Title and Overview stacked on the right */}
-        <div className="text-center md:text-left">
+        <div className="absolute top-0 left-0 w-full p-4 ">
           <div className="text-3xl mb-4">{movieDetail.title}</div>
           <div className="text-xl">{movieDetail.overview}</div>
-          <div className='my-3'> {movieDetail.release_date} </div>
-          <div>  </div>
+          <div className="my-3">
+            {"Yayın tarihi : " + movieDetail.release_date}
+          </div>
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default page
-
-
-
+export default Page;
