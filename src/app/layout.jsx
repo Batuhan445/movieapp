239f91df344bb583aace81ccad5b3e75f12fc557
Batuhan445 <neomatrix44@hotmail.com>
2023-08-01 22:08:02 +0300

    fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./globals.css"
-import Header from "@/components/Header"
+import Header from "@/components/header"
 import Provider from './provider'
 import Tabs from '@/components/Tabs'
 
