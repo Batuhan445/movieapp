73922fcadf6ed8aa4d450cd03ed8325253da9979
Batuhan445 <neomatrix44@hotmail.com>
2023-08-01 17:49:20 +0300

    read format fixed

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,31 +1,33 @@
-import Movies from '@/components/Movies'
-import React from 'react'
+import Movies from "@/components/Movies";
+import React from "react";
 
 const Page = async ({ params }) => {
-    try {
-        const keyword = params.keyword
-        const apiKey = "1d5dad3345b663e244653304e46b496f"
-        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword}`)
-        const data = await res.json()
+  try {
+    const keyword = params.keyword;
+    const apiKey = "1d5dad3345b663e244653304e46b496f";
+    const res = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword}`
+    );
+    const data = await res.json();
 
-        return (
-            <div>
-                {
-                    !data?.results ?
-                        <div>Aradığınız film bulunamadı</div> :
-                        <div className='flex flex-wrap gap-2'>
-                            {data?.results?.map((dt, i) => (
-                                <Movies key={i} dt={dt} />
-                            ))}
-                        </div>
-                }
-            </div>
-        )
-    } catch (error) {
-        // Handle any errors that occur during the API request
-        console.error('Error fetching data:', error);
-        return <div>Something went wrong. Please try again later.</div>;
-    }
-}
+    return (
+      <div>
+        {!data?.results ? (
+          <div>Aradığınız film bulunamadı</div>
+        ) : (
+          <div className="flex flex-wrap">
+            {data?.results?.map((dt, i) => (
+              <Movies key={i} dt={dt} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  } catch (error) {
+    // Handle any errors that occur during the API request
+    console.error("Error fetching data:", error);
+    return <div>Bir hata oluştu. Sonra tekrar deneyiniz</div>;
+  }
+};
 
-export default Page
+export default Page;
